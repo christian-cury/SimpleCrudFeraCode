09fc74a0d6e7e45e81bc857c19cec0d8e0970a43
Christian <contato.mikesantos@gmail.com>
2019-03-23 16:10:04 -0300

    Vue dependencies

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,36 @@
+// import Vue from 'vue';
+// import App from './App.vue';
+
+// Vue.config.productionTip = false;
+
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app');
+
+/* eslint-disable */
+
 import Vue from 'vue';
-import App from './App.vue';
+import VueRouter from 'vue-router';
+import VueAxios from 'vue-axios';
+import moment from 'moment';
+import axios from 'axios';
+import Swal from 'sweetalert2';
+import BootstrapVue from 'bootstrap-vue';
+import VueTheMask from 'vue-the-mask';
 
-Vue.config.productionTip = false;
+Vue.use(VueTheMask)
+Vue.use(VueRouter, VueAxios, axios);
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app');
+Vue.component('kpi-diapers', require('./components/KPI/Diapers.vue'));
+
+
+
+const routes = [
+];
+
+const router = new VueRouter({ mode: 'history', routes: routes});
+
+const app = new Vue({
+    el: '#app',
+    router
+});
